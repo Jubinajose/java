20674f0789df3c900223f799802065bd1770d6c7
Jubinajose <97285100+Jubinajose@users.noreply.github.com>
2022-01-22 22:26:51 +0530

    Update palindrome.java

--- cycle1/palindrome.java
+++ cycle1/palindrome.java
@@ -3,7 +3,7 @@ import java.util.*;
 {
     public static void main(String args[])
 {
-    System.out.println("Enter a number");
+    System.out.println("Enter a three digit number");
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     int rev=0,rem,temp=n;
