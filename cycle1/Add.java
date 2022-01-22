e01b709670cb4668759ac6c3a3412264c3af0929
Jubinajose <97285100+Jubinajose@users.noreply.github.com>
2022-01-22 20:31:51 +0530

    Create Add.java

---
+++ cycle1/Add.java
@@ -0,0 +1,11 @@
+class Add
+{
+    public static void main(String args[])
+    {
+        int a,b,sum;
+        a=6;
+        b=2;
+        sum=a+b;
+        System.out.println("Result="+sum);
+    }
+}
