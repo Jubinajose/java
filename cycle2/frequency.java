721cda96a632bce58831a8fb266794eec1d5f132
Jubinajose <97285100+Jubinajose@users.noreply.github.com>
2022-02-25 18:01:28 +0530

    Update frequency.java

--- cycle2/frequency.java
+++ cycle2/frequency.java
@@ -1,21 +1,22 @@
 import java.util.*;
 class frequency
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int i,x=0;
-		System.out.println("Enter length of the string");
-		int n=sc.nextInt();
-		char s[]=new char[50];
-		System.out.println("Enter the string");
-		for(i=0;i<n;i++)
-			s[i]=sc.next().charAt(0);
-		System.out.println("Enter the character whose frequency is to be claculated");
-		char ch=sc.next().charAt(0);
-		for(i=0;i<n;i++)
-			if(s[i]==ch)
-				x++;
-		System.out.println("Frequency of "+ch+" in the string is "+x);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the String to check the Frequency: ");
+		String a = sc.nextLine();
+		int n = a.length();
+		a = a.toLowerCase();
+		int count = 0,i;
+		System.out.print("Enter the element whose frequency to be checked: ");
+		char x = sc.next().charAt(0);
+		for(i = 0; i<n ; i++)
+		{
+			if(x== a.charAt(i))
+				count++;
+				
+		}
+		System.out.println("The occurence of "+x+" is "+ count);
 	}
 }
