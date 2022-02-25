2386dd8cfcdb278e688f0786137b8d24b7e75088
Jubinajose <97285100+Jubinajose@users.noreply.github.com>
2022-02-25 18:13:17 +0530

    Create Expa.java

---
+++ cycle6/experiment1/Expa.java
@@ -0,0 +1,13 @@
+class Expa
+{
+    public static void main(String[] args) {
+        try
+        {
+            int d=5/0;
+            System.out.println(d);
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
